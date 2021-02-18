d65696019871e7e6b69c165e6bdb288bd3194d0e
Moshe-jpg <mobgadol@gmail.com>
2021-02-18 01:11:58 +0000

    changes for deployment

--- db/migrate/0_create_base_tables.rb
+++ db/migrate/0_create_base_tables.rb
@@ -1,7 +1,7 @@
 class CreateBaseTables < ActiveRecord::Migration
 
   def change
-    create_table :users do |t|
+    create_table :users do |t| 
       t.string :username
       t.string :avatar_url
       t.string :email
@@ -9,7 +9,7 @@ class CreateBaseTables < ActiveRecord::Migration
       t.timestamps
     end
 
-    create_table :finstagram_posts do |t|
+    create_table :finstagram_posts do |t| 
       t.references :user
       t.string :photo_url
       t.timestamps
@@ -26,7 +26,7 @@ class CreateBaseTables < ActiveRecord::Migration
       t.references :user
       t.references :finstagram_post
       t.timestamps
-    end
+    end 
 
   end
 
